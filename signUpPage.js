7e86ed44e98a5d9426593a870e04158c6341c447
Sanket <sanket.jagtap.24.2@mountblue.tech>
2023-07-13 14:43:25 +0530

    wrote the code to redirect to home page

--- signUpPage.js
+++ signUpPage.js
@@ -1,3 +1,19 @@
-document.getElementById("signupbutton").addEventListener("submit",(event)=>{
-    window.location.href= "index.html";
+form.addEventListener('submit',(e)=>{
+        
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const repeatPassword = document.getElementById("repeatPassword").value;
+    const tos = document.getElementById("checkBox").checked;
+
+    e.preventDefault()
+   
+        if (firstName !== "" && lastName !== "" && email !== "" && password !== "" && repeatPassword !== "" && tos) {
+            
+            window.location.href="./index.html";
+        } else {
+            
+            alert("Please fill in all form fields and agree to the Terms of Service.");
+        }
 })
